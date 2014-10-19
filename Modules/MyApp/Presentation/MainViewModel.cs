7a9130114556233e4a528064b27b37fdc1ea092a
Mauro Servienti <mauro@topics.it>
2014-10-19 13:07:29 +0200

    Initial "Modules Samples" commit

---
+++ Modules/MyApp/Presentation/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topics.Radical.Windows.Presentation;
+
+namespace MyApp.Presentation
+{
+    class MainViewModel : AbstractViewModel, MyApp.Contracts.IMainViewModel
+    {
+    }
+}
