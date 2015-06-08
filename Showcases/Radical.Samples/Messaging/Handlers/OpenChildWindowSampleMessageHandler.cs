8389cfd140ca297dd98e2bea5f83e45a6439ea61
Mauro Servienti <mauro@topics.it>
2015-06-08 14:33:18 +0200

    Owner support

--- Showcases/Radical.Samples/Messaging/Handlers/OpenChildWindowSampleMessageHandler.cs
+++ Showcases/Radical.Samples/Messaging/Handlers/OpenChildWindowSampleMessageHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using Topics.Radical.ComponentModel.Messaging;
 using Topics.Radical.Presentation.OpenChildWindow;
 using Topics.Radical.Windows.Presentation.ComponentModel;
@@ -12,17 +13,20 @@ namespace Topics.Radical.Messaging.Handlers
 {
     class OpenChildWindowSampleMessageHandler : AbstractMessageHandler<OpenChildWindowSampleMessage>, INeedSafeSubscription
     {
-        private readonly IViewResolver viewResolver;
+        readonly IViewResolver viewResolver;
+        readonly IConventionsHandler conventions;
 
-        public OpenChildWindowSampleMessageHandler( IViewResolver viewResolver )
+        public OpenChildWindowSampleMessageHandler( IViewResolver viewResolver, IConventionsHandler conventions )
         {
             this.viewResolver = viewResolver;
+            this.conventions = conventions;
         }
 
         public override void Handle( object sender, OpenChildWindowSampleMessage message )
         {
             var view = this.viewResolver.GetView<ChildView>();
-            if (message.AsDialog)
+            view.Owner = this.conventions.GetViewOfViewModel( sender ) as Window;
+            if( message.AsDialog )
             {
                 view.ShowDialog();
             }
